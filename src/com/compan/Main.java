a21a0a61262ff7128eb23579cd433e071f97d958
Dmitriy21IT35 <97725909+Dmitriy21IT35@users.noreply.github.com>
2022-06-07 10:24:45 +0300

    Update Main.java
    Laba18

--- src/com/compan/Main.java
+++ src/com/compan/Main.java
@@ -7,20 +7,57 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner (System.in);
         System.out.println ("Enter numbers:");
-        double x= in.nextDouble ();
+        double x = in.nextDouble ();
         double y = in.nextDouble ();
-        P p = new P(x,y);
-
-
-    }
-}
-   class P {
-    public static double x, y;
-
-    public P(double x,double y) {
-
-        double S = 2 * x + 3 / y;
-        System.out.println ("Result: " + S);
+        P p = P.getP ();
+        p.setP (x, y);
+        p.displayp ();
+        P p1 = P.getP ();
+        if (p.hashCode () == p1.hashCode ()) {
+            System.out.println ("ok");
+        } else {
+            System.out.println ("ERROR");
+        }
     }
 
+    static class P {
+        public static double x, y;
+        static P p = new P (x, y);
+        private static Object P;
+
+        P(double x, double y) {
+            this.x =x;
+            this.y =y;
+        }
+
+        public static P getP() {
+            return p;
+        }
+
+
+        public void setP(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+
+        public double getX() {
+            return x;
+        }
+
+
+        public double getY() {
+            return y;
+        }
+
+        public static Object getInstance() {
+            return P;
+        }
+
+        public void displayp() {
+            double S = 2 * x + 3 / y;
+            System.out.println ("Result:"+S);
+        }
+    }
 }
+
